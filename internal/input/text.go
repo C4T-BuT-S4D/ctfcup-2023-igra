2a62fd669e0d9e5a9ecb20e1396ab969e3760b25
jnovikov <johnnovikov0@gmail.com>
2023-12-06 12:58:13 +0000

    Dialog: autowrap + size

---
+++ internal/input/text.go
@@ -0,0 +1,42 @@
+package input
+
+import (
+	"bufio"
+	"golang.org/x/image/font"
+	"strings"
+)
+
+func WrapLine(t string, face font.Face, width int) (lines []string) {
+	var buf strings.Builder
+
+	scan := bufio.NewScanner(strings.NewReader(t))
+	scan.Split(bufio.ScanWords)
+	for scan.Scan() {
+		word := scan.Text()
+		wnew := font.MeasureString(face, buf.String()+" "+word).Round()
+		if wnew > width {
+			lines = append(lines, buf.String())
+			buf.Reset()
+			buf.WriteString(word)
+		} else {
+			buf.WriteString(" ")
+			buf.WriteString(word)
+		}
+	}
+	if buf.Len() > 0 {
+		lines = append(lines, buf.String())
+	}
+	return
+}
+
+func AutoWrap(t string, face font.Face, width int) string {
+	var lines []string
+
+	lineScan := bufio.NewScanner(strings.NewReader(t))
+	lineScan.Split(bufio.ScanLines)
+	for lineScan.Scan() {
+		lines = append(lines, WrapLine(lineScan.Text(), face, width)...)
+	}
+
+	return strings.Join(lines, "\n")
+}
