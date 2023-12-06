5ee09d8b913128cf1a6338a72b0fff319b909c04
Roman Nikitin <nikrom2012@me.com>
2023-12-06 15:45:52 +0300

    Add password check api

--- cmd/llm-client/main.go
+++ cmd/llm-client/main.go
@@ -38,11 +38,48 @@ func main() {
 	logrus.Infof("started with password %q, model %q", password, model)
 	logrus.Infof("system prompt: %q", fmt.Sprintf(systemPrompt, password))
 
-	type request struct {
+	type passwordRequest struct {
+		Password string `json:"password"`
+	}
+
+	type llmRequest struct {
 		Prompt string `json:"prompt"`
 	}
 
 	e := echo.New()
+	e.POST("/api/check_password", func(c echo.Context) error {
+		team := c.Request().Header.Get("X-Team")
+		if team == "" {
+			logrus.Warnf("request from unknown team: %s", c.Request().RemoteAddr)
+			return c.String(http.StatusForbidden, "Forbidden")
+		}
+
+		logger := logrus.WithFields(logrus.Fields{
+			"request_id":  uuid.NewString(),
+			"remote_addr": c.Request().RemoteAddr,
+			"team":        team,
+			"path":        c.Request().URL.Path,
+		})
+		logger.Info("received request")
+
+		var req passwordRequest
+		if err := c.Bind(&req); err != nil {
+			logger.Errorf("error binding request body: %v", err)
+			return fmt.Errorf("binding request body: %w", err)
+		}
+
+		if req.Password != password {
+			logger.Info("incorrect password %q", req.Password)
+			return c.JSON(http.StatusForbidden, map[string]any{
+				"result": "Incorrect password",
+			})
+		}
+
+		logger.Info("correct password %q", req.Password)
+		return c.JSON(http.StatusOK, map[string]any{
+			"result": "Correct password",
+		})
+	})
 	e.POST("/api/generate", func(c echo.Context) error {
 		team := c.Request().Header.Get("X-Team")
 		if team == "" {
@@ -62,7 +99,7 @@ func main() {
 
 		logger.Info("processing request")
 
-		var req request
+		var req llmRequest
 		if err := c.Bind(&req); err != nil {
 			logger.Errorf("error binding request body: %v", err)
 			return fmt.Errorf("binding request body: %w", err)
