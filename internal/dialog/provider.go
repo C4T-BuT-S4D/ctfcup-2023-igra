f1afbe8ebe5c65245c57fe8b4fdfe7d4d3182b82
jnovikov <johnnovikov0@gmail.com>
2023-12-08 12:40:53 +0000

    Level-1 package

--- internal/dialog/provider.go
+++ internal/dialog/provider.go
@@ -28,20 +28,8 @@ func (sp *StandardProvider) Get(id string) (Dialog, error) {
 	switch id {
 	case "test-npc":
 		return NewDummy("Hello, I'm a test NPC!\n 2 + 2 = ?", "4"), nil
-	case "slonser-task":
-		return NewDummy("Hello, I'm SLONIK and I'm ______ об этом", "pröööh"), nil
 	case "wise-tree-task":
 		return NewWiseTree(), nil
-	case "llm-boss-1":
-		return &LLM{
-			Intro:     "I'm the boss of Arbalest of Siberia. Don't try to defeat me!",
-			Token:     os.Getenv("AUTH_TOKEN"),
-			URL:       "http://localhost:8081",
-			MaskInput: !sp.DisplayInput(),
-		}, nil
-
-	case "capytoshka":
-		return NewCapy(os.Getenv("CAPY_TOKEN")), nil
 	default:
 		return nil, fmt.Errorf("unknown dialog id: %s", id)
 	}
